d70e5771f6d794d050386c4c17871be966f45c45
Horokh Bohdan <horokhbohdandmytrovich@gmail.com>
2025-01-14 14:45:23 +0200

    Specify JDBC while retrieving railway URL

--- src/main/java/dev/ua/ikeepcalm/lumios/database/DataSourceConfig.java
+++ src/main/java/dev/ua/ikeepcalm/lumios/database/DataSourceConfig.java
@@ -19,10 +19,10 @@ public class DataSourceConfig {
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setUrl(environment.getProperty("MYSQL_URL", "jdbc:mysql://localhost:3306/default_db"));
-        dataSource.setUsername(environment.getProperty("MYSQLUSER", "default_user"));
-        dataSource.setPassword(environment.getProperty("MYSQLPASSWORD", "default_password"));
-        dataSource.setDriverClassName(environment.getProperty("spring.datasource.driver-class-name", "com.mysql.cj.jdbc.Driver"));
+        dataSource.setUrl("jdbc:" + environment.getProperty("MYSQL_URL", "jdbc:mysql://localhost:3306/default_db"));
+//        dataSource.setUsername(environment.getProperty("MYSQLUSER", "default_user"));
+//        dataSource.setPassword(environment.getProperty("MYSQLPASSWORD", "default_password"));
+//        dataSource.setDriverClassName(environment.getProperty("spring.datasource.driver-class-name", "com.mysql.cj.jdbc.Driver"));
         return dataSource;
     }
 }
